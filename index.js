db52d38dd46c5d5d4f6859108fbc86d8232e2bfc
Jannis R <mail@jannisr.de>
2018-04-11 12:27:01 +0200

    implement from fasp-server

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 'use strict'
 
-// todo
+const {stat} = require('fs')
+const {randomBytes} = require('crypto')
+const {join} = require('path')
+const {tmpdir} = require('os')
+const {createConnection} = require('net')
+const {spawn} = require('child_process')
+const {MPVClient} = require('mpv-ipc')
+
+const awaitSocket = (path, cb) => {
+	const t = Date.now()
+	const check = () => {
+		stat(path, (err, stats) => {
+			if (err) {
+				if ((Date.now() - t) >= 5000) cb(err)
+				else setTimeout(check, 250)
+			} else {
+				if (stats.isSocket()) return cb()
+				cb(new Error('not a socket'))
+			}
+		})
+	}
+	setTimeout(check, 250)
+}
+
+const defaults = {
+	id: null,
+	socket: null,
+	bin: 'mpv',
+	args: []
+}
+
+const createPlayer = (opt, cb) => {
+	if ('function' === typeof opt) {
+		cb = opt
+		opt = {}
+	}
+	opt = Object.assign({}, defaults, opt)
+
+	const id = opt.id || randomBytes(10).toString('hex')
+	const socketPath = opt.socket || join(tmpdir(), 'mpv-' + id)
+
+	const proc = spawn(opt.bin, opt.args.concat([
+		'--idle=yes',
+		'--quiet',
+		'--input-ipc-server=' + socketPath
+	]), {
+		stdio: ['ignore', 'pipe', 'pipe']
+	})
+	// todo: handle spawn error
+
+	awaitSocket(socketPath, (err) => {
+		if (err) return cb(err)
+
+		const player = new MPVClient(createConnection(socketPath))
+		player.id = id
+		cb(null, player)
+	})
+}
+
+module.exports = createPlayer
